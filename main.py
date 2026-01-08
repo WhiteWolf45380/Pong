2cf5fa9b05bb1e3025927648403f701774966ad2
WhiteWolf45380 <matheodeponte45@gmail.com>
2026-01-08 21:00:23 +0100

    added pygame_managers

--- main.py
+++ main.py
@@ -1,1 +1,16 @@
 import pygame
+from pygame_managers import AudioManager, DataManager, TimeManager, LanguagesManager
+
+
+class Main:
+    def __init__(self):
+        self.managers = {
+            "audio": AudioManager(),
+            "data": DataManager(),
+            "time": TimeManager(),
+            "language": LanguagesManager(),
+        }
+
+
+if __name__ == '__main__':
+    main = Main()
---
+++ pygame_managers/__init__.py
@@ -0,0 +1,5 @@
+from .audio import AudioManager
+from .data import DataManager
+from .time import TimeManager
+from .languages import LanguagesManager
+__all__ = ["AudioManager", "DataManager", "TimeManager", "LanguagesManager"]
